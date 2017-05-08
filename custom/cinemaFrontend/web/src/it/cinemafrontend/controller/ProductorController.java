e7bb2feecb1e2663594c92abd3203a11324e17a6
Angela Peluso <angelapeluso@inwind.it>
2017-05-08 22:13:10 +0200

    Controller e jsp Productor

---
+++ custom/cinemaFrontend/web/src/it/cinemafrontend/controller/ProductorController.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package it.cinemafrontend.controller;
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.cinemafacade.data.ProductorData;
+import it.cinemafacade.facades.ProductorFacade;
+
+
+@Controller
+public class ProductorController
+{
+	@Autowired
+	private ProductorFacade productorFacade;
+
+	@RequestMapping(value = "/productors/{productorCode}")
+	public String showProductorDetails(@PathVariable String productorCode, final Model model) throws UnsupportedEncodingException
+	{
+		productorCode = URLDecoder.decode(productorCode, "UTF-8");
+		final ProductorData productor = productorFacade.getProductor(productorCode);
+		model.addAttribute("productor", productor);
+		return "ProductorDetails";
+	}
+
+	@Autowired
+	public void setFacade(final ProductorFacade facade)
+	{
+		this.productorFacade = facade;
+	}
+
+}
