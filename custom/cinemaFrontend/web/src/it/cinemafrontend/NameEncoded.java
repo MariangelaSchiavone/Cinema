e9aa8f5430030a4017ef2484684ef56ee957ad0f
Carmine Di Gruttola <c.digruttola@hotmail.it>
2017-05-08 22:12:10 +0200

    Cinema Controller

---
+++ custom/cinemaFrontend/web/src/it/cinemafrontend/NameEncoded.java
@@ -0,0 +1,25 @@
+package it.cinemafrontend;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.log4j.Logger;
+
+
+public class NameEncoded
+{
+	private final static Logger LOG = Logger.getLogger(NameEncoded.class);
+
+	public static String getNameEncoded(final String name)
+	{
+		try
+		{
+			return URLEncoder.encode(name, "UTF-8");
+		}
+		catch (final UnsupportedEncodingException e)
+		{
+			LOG.error("Problems while encoding", e);
+			return "";
+		}
+	}
+}
