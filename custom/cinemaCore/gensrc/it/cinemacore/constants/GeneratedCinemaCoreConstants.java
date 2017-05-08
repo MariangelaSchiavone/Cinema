3ca904307da46ca893c35f99e063fc96a07cb369
Carmine Di Gruttola <c.digruttola@hotmail.it>
2017-05-08 12:52:44 +0200

    Repository initialized

---
+++ custom/cinemaCore/gensrc/it/cinemacore/constants/GeneratedCinemaCoreConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 8-mag-2017 12.52.01                         ---
+ * ----------------------------------------------------------------
+ */
+package it.cinemacore.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCinemaCoreConstants
+{
+	public static final String EXTENSIONNAME = "cinemaCore";
+	
+	protected GeneratedCinemaCoreConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
