c8759c7519a7f90fc4fb01cefbd84da3750be8fa
Carmine Di Gruttola <c.digruttola@hotmail.it>
2017-05-08 12:33:09 +0200

    Repository initialized

---
+++ custom/cinemaCore/src/it/cinemacore/constants/CinemaCoreConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package it.cinemacore.constants;
+
+/**
+ * Global class for all CinemaCore constants. You can add global constants for your extension into this class.
+ */
+public final class CinemaCoreConstants extends GeneratedCinemaCoreConstants
+{
+	public static final String EXTENSIONNAME = "cinemaCore";
+
+	private CinemaCoreConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ custom/cinemaFrontend/web/webroot/index.jsp
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1>cinemaFrontend</h1>
+		Welcome to my extension.
+	</body>
+</html>
+
